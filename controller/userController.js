34603c0972ae34e7501fcd7cba0fca5ad6540aff
rizwanc018 <22193871+rizwanc018@users.noreply.github.com>
2023-09-21 18:18:27 +0530

    prodcut fetch, search,sort, filter

--- controller/userController.js
+++ controller/userController.js
@@ -1,22 +1,23 @@
 const User = require('../model/userModel')
 const jwt = require('jsonwebtoken')
+const Product = require('../model/productModel')
 
 const userLogin = async (req, res) => {
     try {
         const { mobile } = req.body
-        const userData = await User.findOne({ mobile:mobile })
+        const userData = await User.findOne({ mobile: mobile })
         if (userData) {
-                const token = jwt.sign({ id: userData._id, role: "user" }, process.env.JWT_SECRET_KEY, { expiresIn: '1d' })
-                const data = token
-                res.status(200).send({ success: true, message: "login successfull", data })
+            const token = jwt.sign({ id: userData._id, role: "user" }, process.env.JWT_SECRET_KEY, { expiresIn: '1d' })
+            const data = token
+            res.status(200).send({ success: true, message: "login successfull", data })
         } else {
             const newUser = new User({
                 mobile
             })
-            const saveUserData=await newUser.save()
+            const saveUserData = await newUser.save()
             const token = jwt.sign({ id: saveUserData._id, role: "user" }, process.env.JWT_SECRET_KEY, { expiresIn: '1d' })
-            const data=token
-            res.status(201).send({ success: true, message: "account created successfully", data})
+            const data = token
+            res.status(201).send({ success: true, message: "account created successfully", data })
         }
     } catch (error) {
         console.log(error.message);
@@ -24,6 +25,29 @@ const userLogin = async (req, res) => {
     }
 }
 
+const getProducts = async (req, res) => {
+    try {
+        const sort = req.query.sort ?? 'default'
+        const search = req.query.name ?? '.*'
+        const category = req.query.categoryId ?? ''
+
+        const sortOption = {}
+        if (sort === 'lowToHigh') sortOption.price = 1
+        else if (sort === 'highToLow') sortOption.price = -1
+
+        const query = { isBlocked: false, }
+        if (search) query.name = { $regex: search, $options: 'i' }
+        if (category) query.category = category
+
+        const products = await Product.find(query).sort(sortOption)
+        res.status(200).send({ success: true, message: "Products fetched successfully", data: products })
+    } catch (error) {
+        console.log(error.message);
+        res.status(500).send({ success: false, message: "Error fetching products" })
+    }
+}
+
 module.exports = {
     userLogin,
+    getProducts
 }
--- routes/user.js
+++ routes/user.js
@@ -4,6 +4,7 @@ const userController = require('../controller/userController')
 
 
 userRoute.post('/userLogin',userController.userLogin)
+userRoute.get('/products', userController.getProducts)
 
 
 module.exports=userRoute;
