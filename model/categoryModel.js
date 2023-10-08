8b97716b1e2601a8dce94777de4f2d9eaa51e0d1
Amal-Suresh <amalsureshms2001@gmail.com>
2023-10-08 12:24:58 +0530

    removed unwanted commands

--- model/categoryModel.js
+++ model/categoryModel.js
@@ -1,21 +1,3 @@
-// const mongoose = require('mongoose')
-
-// const categorySchema =mongoose.Schema({
-//     name:{
-//         type:String,
-//         required:true  
-//     },
-//     image:{
-//         type:String,
-//         required:true
-//     },
-//     isBlocked:{
-//         type:Boolean,
-//         default:true
-//     }
-// })
-// module.exports = mongoose.model("category",categorySchema)
-
 const mongoose = require('mongoose')
 
 const categorySchema =mongoose.Schema({
