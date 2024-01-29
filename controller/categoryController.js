5396b0495d6d8ef9e5af3edd6af6f07d8d7b6eb6
Amal-Suresh <amalsureshms2001@gmail.com>
2024-01-29 23:09:47 +0530

    updated add category api

--- controller/categoryController.js
+++ controller/categoryController.js
@@ -19,8 +19,12 @@ const addCategory = async (req, res) => {
             name: req.body.name,
             image: { image: image, public_id: public_id }
         })
-        CategoryDetails.save()
-        res.status(200).json({ message: "successfully category added" })
+        const addCat = await CategoryDetails.save()
+        if (addCat) {
+         return   res.status(200).json({success:true, message: "successfully category added" })
+        } else {
+         return   res.status(400).json({success:false, message: "failed to add category" })
+        }
     } catch (error) {
         console.log("error : ", error.message)
     }
@@ -31,16 +35,16 @@ const editCategory = async (req, res) => {
     try {
         let image;
         let public_id;
-        if (req.file){
-            const CategoryData = await Category.findOne({_id:req.query.id})
-            if(CategoryData.image.public_id){
+        if (req.file) {
+            const CategoryData = await Category.findOne({ _id: req.query.id })
+            if (CategoryData.image.public_id) {
                 cloudinary.uploader
-                .destroy(CategoryData.image.public_id)
+                    .destroy(CategoryData.image.public_id)
             }
             const result = await cloudinary.uploader.upload(req.file.path, { folder: 'category Image' });
             image = result.secure_url
             public_id = result.public_id
-            const updatedCategory = await Category.updateOne({ _id: req.query.id }, { $set: { name: req.body.name,  'image.image': req.body.image ,"image.public_id":public_id} })
+            const updatedCategory = await Category.updateOne({ _id: req.query.id }, { $set: { name: req.body.name, 'image.image': req.body.image, "image.public_id": public_id } })
         } else {
             const updatedCategory = await Category.updateOne({ _id: req.query.id }, { $set: { name: req.body.name } })
         }
@@ -99,8 +103,8 @@ const getCategory = async (req, res) => {
     try {
         console.log("entered getCategory")
         const getCategory = await Category.find()
-        console.log("getCategory : ",getCategory)
-        res.status(200).json({ message: "successfully deleted",getCategory})
+        console.log("getCategory : ", getCategory)
+        res.status(200).json({ message: "successfully deleted", getCategory })
     } catch (error) {
         console.log("error : ", error.message)
 
